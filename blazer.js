036ba60a9dd9d4bd04c35dff8663526fa6f8a61a
harmadyaalghazali-sudo <harmadya.alghazali@gmail.com>
2026-04-22 23:11:40 +0700

    Add geoOptions for location accuracy settings

---
+++ blazer.js
@@ -0,0 +1,5 @@
+const geoOptions = {
+    enableHighAccuracy: true, // Paksa pakai GPS satelit
+    timeout: 15000,           // Kasih waktu 15 detik buat nyari sinyal terbaik
+    maximumAge: 0             // Haram pakai data lokasi lama (cache)
+};
